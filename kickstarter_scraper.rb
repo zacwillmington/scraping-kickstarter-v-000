84f02d6de2dd92ddb6a19075797f4db307fb3090
Zac Willmington <zac.willmington@gmail.com>
2018-03-02 02:16:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,7 +19,8 @@ def create_project_hash
     projects = {}
 
     kickstarter.css("li.project.grid_4").each do |project|
-        projects[project] = {}
+        title = project.css("h2.bbcard_name strong a").text
+        projects[title.to_sym]
     end
 
     projects
