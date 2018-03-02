43188c68ad4e08c4c93e369be945abf7ce6d0e95
Zac Willmington <zac.willmington@gmail.com>
2018-03-02 02:12:47 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,29 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
+
+# projects: kickstarter.css("li.project.grid_4")
+
+# title: project.css("h2.bbcard_name strong a").text
+
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+
+#BLURB
+#project.css("p.bbcard_blurb").text
+
 
 def create_project_hash
-  # write your code here
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+
+    projects = {}
+
+    kickstarter.css("li.project.grid_4").each do |project|
+        projects[project] = {}
+    end
+
+    projects
+    #binding.pry
 end
+
+create_project_hash
